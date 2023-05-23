fdfb527bd59d642898d321aaa05e95ff84bdc924
Alexis Florentin <alexisflorentin14@gmail.com>
2023-05-23 20:43:59 +0200

    fianal

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,31 +1,37 @@
 class BookmarksController < ApplicationController
-  #before_action :set_list, only: %i[new create destroy]
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
-  def destroy
-    @bookmark = Bookmark.find(params[:id])
-    @bookmark.destroy
-    # No need for app/views/restaurants/destroy.html.erb
-    redirect_to list_path, status: :see_other
-  end
-
   def create
-    @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new(list_params)
+    @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
 
-  def list_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
-  belongs_to :list
   belongs_to :movie
-  validates :list_id, :movie_id, presence: true
-  validates_uniqueness_of :list_id, scope: [:movie_id]
-  validates :comment, length: { minimum: 6 }
+  belongs_to :list
+
+  validates :comment, length: {minimum: 6}
+  validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
 end
